57a597b2d8ff319a27374b3d91c4ca8debc283ca
iamjoel16 <eljoel1912@hotmail.com>
2024-11-05 23:16:08 -0400

    Añadido de información del Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import './Footer.css';
 
-function Footer() {
+const Footer = () => {
     return (
         <footer>
-            <p>© 2024 UTESA - Todos los derechos reservados.</p>
+            <div className="footer-logo">
+                <a href="/">
+                    <img src="/images/utesa-logo.png" alt="UTESA Logo" className="logo-image" />
+                </a>
+            </div>
+            
+            <div className="footer-info">
+                <p>Sistema Corporativo Universidad Tecnológica de Santiago | UTESA</p>
+            </div>
+
+            <div className="footer-contact">
+                <p>Contáctanos:</p>
+                <p>(809) 221 - 6221</p>
+                <button className="email-button" onClick={() => window.location.href = 'mailto:prueba@utesa.com'}>
+                    Vía Correo
+                </button>
+            </div>
+           
         </footer>
     );
-}
+};
 
 export default Footer;
